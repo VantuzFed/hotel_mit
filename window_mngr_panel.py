f4d81d54449532ae4101fb403a6acbba2bb278cb
vantuzfed <ivan238777@gmail.com>
2024-12-04 23:50:35 +0300

    too much to describe

--- window_mngr_panel.py
+++ window_mngr_panel.py
@@ -2,13 +2,13 @@ from PyQt6.QtWidgets import QWidget
 from PyQt6 import uic
 from PyQt6.QtCore import pyqtSignal
 
-class EmpPanel(QWidget):
-    but_tab_sig = pyqtSignal()
-    but_reg_sig = pyqtSignal()
+class ManagPanel(QWidget):
+    but_edt_sig = pyqtSignal()
+    but_reg_m_sig = pyqtSignal()
     but_book_sig = pyqtSignal()
     def __init__(self, main_w, db):
-        super(EmpPanel,self).__init__()
-        self.ui = uic.loadUi('gui_files/emplo_main.ui',self)
+        super(ManagPanel,self).__init__()
+        self.ui = uic.loadUi('gui_files/manag_main.ui',self)
         self.main_w = main_w
         self.obj = db
         self.ui.but_tab.clicked.connect(self.but_tab_f)
@@ -19,7 +19,7 @@ class EmpPanel(QWidget):
         self.but_tab_sig.emit()
 
     def but_reg_cli_f(self):
-        self.but_reg_sig.emit()
+        self.but_tab_sig.emit()
 
     def but_book_f(self):
-        self.but_book_sig.emit()
+        self.but_tab_sig.emit()
